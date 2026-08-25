dc09bc73bf877b585f17a85c771b5fd84524900a
Seripart <tosapon.siri@bumail.net>
2026-08-26 03:08:12 +0700

    Add 4 character

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -39,11 +39,118 @@ namespace Lab02
             Console.WriteLine();
             //ลด HP 60 หน่วย
             currentHp = currentHp - 60;
-            Console.WriteLine();
             Console.WriteLine("===== BOSS STATUS: INITIAL =====");
             Console.WriteLine($"HP {currentHp} / {maxHp}");
             hpPercent = currentHp * 100 / maxHp;
             Console.WriteLine($"HP Percent : {hpPercent}%");
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("==== Flower List ====");
+            string flowerName1 = "Rose";
+            int relationshipTool1 = 1;
+            char rarity1 = 'C';
+            Console.WriteLine($"Flower: {flowerName1}");
+            Console.WriteLine($"Relationship poit: {relationshipTool1}");
+            Console.WriteLine($"Rarity: {rarity1}");
+            string flowerName2 = "Sunflower";
+            int relationshipTool2 = 5;
+            char rarity2 = 'S';
+            Console.WriteLine($"Flower: {flowerName2}");
+            Console.WriteLine($"Relationship poit: {relationshipTool2}");
+            Console.WriteLine($"Rarity: {rarity2}");
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("==== Character ====");
+            string name1 = "Emma";
+            string personality1 = "Friendly";
+            string gender1 = "Female";
+            bool status1 = true;
+            int currentRelationship1 = 6;
+            int maxRelationship1 = 10;
+            int high1 = 168;
+            int weight1 = 46;
+            float STR1 = 12.5f;
+            float INT1 = 47.5f;
+            double monney1 = 46.6666;
+            Console.WriteLine($"Name: {name1}");
+            Console.WriteLine($"Personality: {personality1}");
+            Console.WriteLine($"Gender: {gender1}");
+            Console.WriteLine($"Can make relation: {status1}");
+            Console.WriteLine($"Relationship: {currentRelationship1}/{maxRelationship1}");
+            Console.WriteLine($"High: {high1} Weight: {weight1}");
+            Console.WriteLine($"STR: {STR1} INT: {INT1}");
+            Console.WriteLine($"Have monneys: {monney1}$");
+            Console.WriteLine($"Give Rose -----> {currentRelationship1 + relationshipTool1 }/{maxRelationship1}");
+            Console.WriteLine();
+
+            string name2 = "Linda";
+            string personality2 = "Strict";
+            string gender2 = "Female";
+            bool status2 = true;
+            int currentRelationship2 = 1;
+            int maxRelationship2 = 10;
+            int high2 = 178;
+            int weight2 = 70;
+            float STR2 = 32.5f;
+            float INT2 = 50.5f;
+            double monney2 = 102.2222;
+            Console.WriteLine($"Name: {name2}");
+            Console.WriteLine($"Personality: {personality2}");
+            Console.WriteLine($"Gender: {gender2}");
+            Console.WriteLine($"Can make relation: {status2}");
+            Console.WriteLine($"Relationship: {currentRelationship2}/{maxRelationship2}");
+            Console.WriteLine($"High: {high2} Weight: {weight2}");
+            Console.WriteLine($"STR: {STR2} INT: {INT2}");
+            Console.WriteLine($"Have monneys: {monney2}$");
+            Console.WriteLine($"Give Rose -----> {currentRelationship2 + relationshipTool1}/{maxRelationship2}");
+            Console.WriteLine();
+
+            string name3 = "Roy";
+            string personality3 = "Energetic";
+            string gender3 = "Male";
+            bool status3 = true;
+            int currentRelationship3 = 8;
+            int maxRelationship3 = 10;
+            int high3 = 172;
+            int weight3 = 56;
+            float STR3 = 20.5f;
+            float INT3 = 35.5f;
+            double monney3 = 29.9999;
+            Console.WriteLine($"Name: {name3}");
+            Console.WriteLine($"Personality: {personality3}");
+            Console.WriteLine($"Gender: {gender3}");
+            Console.WriteLine($"Can make relation: {status3}");
+            Console.WriteLine($"Relationship: {currentRelationship3}/{maxRelationship3}");
+            Console.WriteLine($"High: {high3} Weight: {weight3}");
+            Console.WriteLine($"STR: {STR3} INT: {INT3}");
+            Console.WriteLine($"Have monneys: {monney3}$");
+            Console.WriteLine($"Give Sunflower -----> {currentRelationship3 + relationshipTool2}/{maxRelationship3}");
+            Console.WriteLine();
+
+            string name4 = "Tyler";
+            string personality4 = "Irritable";
+            string gender4 = "Male";
+            bool status4 = true;
+            int currentRelationship4 = 0;
+            int maxRelationship4 = 10;
+            int high4 = 192;
+            int weight4 = 88;
+            float STR4 = 79.5f;
+            float INT4 = 12.5f;
+            double monney4 = 0.00012;
+            Console.WriteLine($"Name: {name4}");
+            Console.WriteLine($"Personality: {personality4}");
+            Console.WriteLine($"Gender: {gender4}");
+            Console.WriteLine($"Can make relation: {status4}");
+            Console.WriteLine($"Relationship: {currentRelationship4}/{maxRelationship4}");
+            Console.WriteLine($"High: {high4} Weight: {weight4}");
+            Console.WriteLine($"STR: {STR4} INT: {INT4}");
+            Console.WriteLine($"Have monneys: {monney4}$");
+            Console.WriteLine($"Give Sunflower -----> {currentRelationship4 + relationshipTool2}/{maxRelationship4}");
+
+
         }
     }
 }
