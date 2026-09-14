ecc6b7b40c81655fd1128f3cc6bb34b3151681f6
Seripart <tosapon.siri@bumail.net>
2026-09-14 22:00:52 +0700

    Change UI design

--- Assignment01/Program.cs
+++ Assignment01/Program.cs
@@ -14,7 +14,7 @@ namespace Assignment01
     {
         static void Main(string[] args)
         {
-            const string idCard = "Soldier ID Card";
+            const string IdCard = "Soldier ID Card";
             var armyRank = "Sergeant";
             var charracterName = "Jone";
             var unit = '7';
@@ -25,15 +25,40 @@ namespace Assignment01
             double performance = 69.9;
             bool isAlive = true;
 
-            Console.WriteLine($"{idCard}");
-            Console.WriteLine($"{armyRank} {charracterName}");
-            Console.WriteLine($"Unit : {unit}");
-            Console.WriteLine($"Height : {height}");
-            Console.WriteLine($"Weight : {weight}");
-            Console.WriteLine($"Accuracy : {accuracy}");
-            Console.WriteLine($"Mission Success Rate : {missionSuccessRate}");
-            Console.WriteLine($"Performance : {performance}");
-            Console.WriteLine($"Alive : {isAlive}");
+            Console.WriteLine(":<:>:<:>:<:>:<:>:<:>:<:>:<:>:<:>:<:>:");
+            Console.WriteLine("|                                   |");
+            Console.WriteLine("|              _.---``'-.           |"); 
+            Console.WriteLine("|             '._ __<>_/            |");
+            Console.WriteLine("|               |'--.__\\            |");
+            Console.WriteLine("|              (    _\\|             |");
+            Console.WriteLine("|               |   _ |             |");
+            Console.WriteLine("|               )\\___/              |");
+            Console.WriteLine("|           .--'`:._]               |");
+            Console.WriteLine("|          /         '-.            |");
+            Console.WriteLine("|->->->->->->->->->->->->->->->->->-|");
+            Console.WriteLine($"|          {IdCard}          |");
+            Console.WriteLine($"|                                   |");
+            Console.WriteLine($"| {armyRank} {charracterName}                     |");
+            Console.WriteLine($"| Unit : {unit}                          |");
+            Console.WriteLine($"| Height : {height}                      |");
+            Console.WriteLine($"| Weight : {weight}                       |");
+            Console.WriteLine($"| Accuracy : {accuracy}                     |");
+            Console.WriteLine($"| Mission Success Rate : {missionSuccessRate}       |");
+            Console.WriteLine($"| Performance : {performance}                |");
+            Console.WriteLine($"| Alive : {isAlive}                      |");
+            Console.WriteLine($"|                                   |");
+
+            Console.WriteLine($"| Average Status                    |");
+            double accuracyAsDouble = accuracy;
+            Console.WriteLine($"| Avg. Accuracy (Implicit) : {accuracyAsDouble}     |");
+
+            int performanceTruncated = (int)performance;
+            int performanceRounded = Convert.ToInt32(performance);
+            Console.WriteLine($"| Avg. Performance (Truncated) : {performanceTruncated} |");
+            Console.WriteLine($"| Avg. Performance (Rounded) : {performanceRounded}   |");
+
+            Console.WriteLine($"|                                   |");
+            Console.WriteLine(":<:>:<:>:<:>:<:>:<:>:<:>:<:>:<:>:<:>:");
 
         }
     }
