1c26a2ac604aa50e43011e97f7c5e911840f14fe
Seripart <tosapon.siri@bumail.net>
2026-09-22 22:53:20 +0700

    Assignments complete

--- Lab06/Program.cs
+++ Lab06/Program.cs
@@ -66,52 +66,134 @@ namespace Lab06
             }
             */
 
-            Console.WriteLine("Your found a zombie, Think if you donn't wanna die!");
-            Console.WriteLine("So badddd, You gun magazine is empty");
-            Console.WriteLine("Choice 1 : Reload magazine.");
-            Console.WriteLine("Choice 2 : Just shoot damn it.");
-            Console.WriteLine("Choice 3 : Kang fo kick!");
+            /*
+             You found zombie!
+             Sadly, You donn't have any ammo in magazine...
+             choose you decision
 
-            Console.WriteLine("Chose your decision: ");
-            bool inputValid = int.TryParse(Console.ReadLine(), out int choice);
-            if (!inputValid || choice < 1 || choice > 3)
+                1.Wait! Lemme reload.
+                Now you have full ammo in magazine.
+                What are you do next.
+                        1.1 Pull the trigger asap!
+                               Rat-a-tat-tat Rat-tat-tat, Now zombie look like a beehive...
+                               You well done.
+                        1.2 Calmly aim.
+                               The man who waits for the perfect shot dies with a full magazine...
+                               You dead.
+
+                2.Just shoot damn it!
+                Click! Click, Guest what?
+                You dead.
+
+                3.Pick up sashimi knife.
+                        3.1 Stab
+                                Crunch!
+                        It’s almost dead. Just one more hit.
+                        3.2 Throw
+                                Miss!
+                                Sorry,You don't have a single ninja in your bloodline...
+                                You dead.
+
+                4.Kang fu.
+                        4.1 You're not even Bruce Lee...
+                        4.2 Woah how you do that!
+                            Zombie heah roll on the ground.
+                            Looks like luck is on your side today, You well done.
+            */
+
+            int zombieHp = 10;
+            int knifeAtk = 5;
+
+            Console.WriteLine("You found zombie!");
+            Console.WriteLine("Sadly, You donn't have any ammo in magazine...");
+            Console.WriteLine("Go, Choose you decision.");
+            Console.WriteLine("1. Wait! Lemme reload.");
+            Console.WriteLine("2. Just shoot damn it!");
+            Console.WriteLine("3. Pick up sashimi knife.");
+            Console.WriteLine("4. Kang fu.");
+            Console.Write("\nChoose your decision (1-4): ");
+            bool inputValid = int.TryParse(Console.ReadLine(), out int mainChoice);
+            if (!inputValid || mainChoice < 1 || mainChoice > 4)
             {
-                Console.WriteLine("Donn't have another change, You dead.");
+                Console.WriteLine("\nDonn't have another change...");
+                Console.WriteLine("You dead.");
+            }
+            else if (mainChoice == 1)
+            {
+                Console.WriteLine("Now you have full ammo in magazine.");
+                Console.WriteLine("What are you do next.");
+                Console.WriteLine("1. Pull the trigger ASAP!");
+                Console.WriteLine("2. Calmly aim.");
+                Console.Write("Choose your decision (1-2): ");
+                bool inputValid01 = int.TryParse(Console.ReadLine(), out int sideChoice01);
+                if (!inputValid01 || sideChoice01 < 1 || sideChoice01 > 2)
+                {
+                    Console.WriteLine("\nDonn't have another change...");
+                    Console.WriteLine("You dead.");
+                }
+                else if (sideChoice01 == 1)
+                {
+                    Console.WriteLine("\nRat-a-tat-tat! Rat-tat-tat!");
+                    Console.WriteLine("Now zombie look like a beehive...");
+                    Console.WriteLine("You well done.");
+                }
+                else if (sideChoice01 == 2)
+                {
+                    Console.WriteLine("\nThe man who waits for the perfect shot dies with a full magazine...");
+                    Console.WriteLine("You dead.");
+                }
             }
 
-            else if (choice == 1)
+            else if (mainChoice == 2)
             {
-                Console.WriteLine("Now you have a full ammo, What you do next.");
-                Console.WriteLine("Choice 1 : Pull the trigger.");
-                Console.WriteLine("Choice 2 : Aim calmly.");
-
-                Console.WriteLine("Chose your decision: ");
-                bool inputValid01 = int.TryParse(Console.ReadLine(), out int choice02);
-                if (!inputValid01 || choice02 < 1 || choice02 > 2)
-                {
-                    Console.WriteLine("Think fast! Now you dead");
-                }
-                else if (choice02 == 1)
-                {
-                    Console.WriteLine("Grrrrr! Grrrrr!, You just drum mag and now zombie look like beehive.");
-                    Console.WriteLine("Phewww, you survive this day.");
-                }
-                else if (choice02 == 2)
-                {
-                    Console.WriteLine("The man who waits for the perfect shot dies with a full magazine.");
-                }
-            }
-
-            else if (choice == 2)
-            {
-                Console.WriteLine("Click! Click! Guest what?");
+                Console.WriteLine("\nClick! Click, Guest what?");
                 Console.WriteLine("You dead.");
             }
 
-            else if (choice == 3)
+            else if (mainChoice == 3)
             {
-                Console.WriteLine("You aren't Bruce Lee");
-                Console.WriteLine("You dead.");
+                Console.WriteLine("what are you gonna do");
+                Console.WriteLine("1. Stab.");
+                Console.WriteLine("2. Throw knife.");
+                Console.Write("\nChoose your decision (1-2): ");
+                bool inputValid03 = int.TryParse(Console.ReadLine(), out int sideChoice03);
+                if (sideChoice03 == 1)
+                {
+                    zombieHp -= knifeAtk;
+                    if (zombieHp > 0)
+                    {
+                        Console.WriteLine("There's no way to kill it...");
+                        Console.WriteLine("You dead.");
+                    }
+                    else if (zombieHp <= 0)
+                    {
+                        Console.WriteLine("\nZombie head rolled on the floor,And blood splattered all over the room.");
+                        Console.WriteLine("You well done.");
+                    }
+                }
+                else if (sideChoice03 == 2)
+                {
+                    Random randomCrit = new Random();
+                    int roll = randomCrit.Next(1, 4);
+                    bool crit = roll == 1;
+                    Console.WriteLine($"A 1/3 chance, and you got: {roll}");
+                    if (crit)
+                    {
+                        Console.WriteLine("Looks like luck is on your side today.");
+                        Console.WriteLine("You well done.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You don't have a single ninja in your bloodline sir.");
+                        Console.WriteLine("Now you dead.");
+                    }
+                }
+            }
+
+            else if (mainChoice == 4)
+            {
+                Console.WriteLine("What!? You not even Bruce Lee.");
+                Console.WriteLine("Now you dead.");
             }
         }
     }
