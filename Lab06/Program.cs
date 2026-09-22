a4f5b0b18f60350492983e2af680597f74c46a52
TosaponBU <tosapon.siri@bumail.net>
2026-09-22 11:41:18 +0700

    Assignment

--- Lab06/Program.cs
+++ Lab06/Program.cs
@@ -12,6 +12,7 @@ namespace Lab06
     {
         static void Main(string[] args)
         {
+            /*
             int lives = 1;
 
             if (lives <= 0) // ใส่เงื่อนไข ค่าที่ได้ต้องเป็น bool
@@ -63,7 +64,50 @@ namespace Lab06
             {
                 Console.WriteLine("You Lives");
             }
+            */
 
+            Console.WriteLine("Your found a zombie, Think if you donn't wanna die!");
+            Console.WriteLine("So badddd, You gun magazine is empty");
+            Console.WriteLine("Choice 1 : Reload magazine.");
+            Console.WriteLine("Choice 2 : Just shoot damn it.");
+            Console.WriteLine("Choice 3 : Kang fo kick!");
+
+            Console.WriteLine("Chose your decision: ");
+            bool inputValid = int.TryParse(Console.ReadLine(), out int choice);
+            if (!inputValid || choice < 1 || choice > 3)
+            {
+                Console.WriteLine("Donn't have another change, You dead.");
+            }
+
+            else if (choice == 1)
+            {
+                Console.WriteLine("Now you have a full ammo, What you do next.");
+                Console.WriteLine("Choice 1 : Pull the trigger.");
+                Console.WriteLine("Choice 2 : Aim calmly.");
+
+                Console.WriteLine("Chose your decision: ");
+                bool inputValid01 = int.TryParse(Console.ReadLine(), out int choice02);
+                if (!inputValid01 || choice02 < 1 || choice02 > 2)
+                {
+                    Console.WriteLine("Think fast! Now you dead");
+                }
+                else if (choice02 == 1)
+                {
+                    Console.WriteLine("Grrrrr! Grrrrr!, You just drum mag and now zombie look like beehive.");
+                    Console.WriteLine("Phewww, you survive this day.");
+                }
+                else if (choice02 == 2)
+                {
+                    Console.WriteLine("The man who waits for the perfect shot dies with a full magazine.");
+                }
+            }
+
+            else if (choice == 2)
+            {
+                Console.WriteLine("Click! Click! Guest what?");
+                Console.WriteLine("You dead.");
+            }
+                    
         }
     }
 }
